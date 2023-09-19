050a2b58075e526876ffbdf3c1fbed11d819c5a8
Mixailka <88591984+MKSO4KA@users.noreply.github.com>
2023-09-19 17:09:14 +0300

    ReUpload main branch)))

---
+++ PaletteCreator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PixelArt
+{
+    internal static class Palette
+    {
+
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+            //Application.Run(new PixelArtVisual());
+            //Console.WriteLine("assdsaa");
+        }
+
+    }
+}
+
